712145ee3f54b0d9520ecb223be2850bac9057f0
Keith W. Campbell <keithc@ca.ibm.com>
2021-08-24 13:41:31 -0400

    Update versions and branches to build 0.28.0
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -57,10 +57,10 @@ declare -A shas
 declare -A references
 
 git_urls[openj9]=https://github.com/eclipse-openj9/openj9
-branches[openj9]=master
+branches[openj9]=v0.28.0-release
 
 git_urls[omr]=https://github.com/eclipse-openj9/openj9-omr
-branches[omr]=openj9
+branches[omr]=v0.28.0-release
 
 pflag=false
 
