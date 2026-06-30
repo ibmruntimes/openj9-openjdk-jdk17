2f3ecb14ca67a7aee9dd253324ab6c0314fdf48d
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2026-06-30 13:44:01 -0400

    Allow aliases in RestrictedSecurity constraints
    When defining constraints for specific services allowed
    by specific providers, one can now use an alias instead
    of the original algorithm name.
    
    A test case has, also, been added to verify said
    functionality.
    
    Signed-off-by: Kostas Tsiounis <kostas.tsiounis@ibm.com>

--- src/java.base/share/classes/java/security/Provider.java
+++ src/java.base/share/classes/java/security/Provider.java
@@ -25,7 +25,7 @@
 
 /*
  * ===========================================================================
- * (c) Copyright IBM Corp. 2022, 2025 All Rights Reserved
+ * (c) Copyright IBM Corp. 2022, 2026 All Rights Reserved
  * ===========================================================================
  */
 
@@ -1402,7 +1402,7 @@ public abstract class Provider extends Properties {
             throw new IllegalArgumentException
                     ("service.getProvider() must match this Provider object");
         }
-        if (!RestrictedSecurity.canServiceBeRegistered(s)) {
+        if (!RestrictedSecurity.canServiceBeRegistered(s, s.getAliases())) {
             // We're in restricted security mode which does not allow this service,
             // return without registering.
             return;
